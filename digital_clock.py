cb97b471c91583d5a8b218337e13089c5ee73c12
Ankur Gajurel <ankurgajurel02@gmail.com>
2022-08-14 17:17:34 +0545

    guesswhat

---
+++ digital_clock.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("SL DevCode Clock")
+
+def time():
+    string = strftime('%H:%M:%S: %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = Label(root , font=("ds-digital", 200), background="black", foreground="green")
+label.pack(anchor='center')
+time()
+mainloop()
